0e458273db2dbbeaddb5f4834f48dc0280d97ec8
Dev-Am12 <ashutosh.am1208@gmail.com>
2026-08-01 00:23:49 +0530

    rust init

---
+++ rJSON/target/debug/deps/rjson.d
@@ -0,0 +1,7 @@
+C:\Users\ashu1\Coding Projects\PortMortem cJSON\cJSONtoRust\rJSON\target\debug\deps\rjson.d: src\lib.rs
+
+C:\Users\ashu1\Coding Projects\PortMortem cJSON\cJSONtoRust\rJSON\target\debug\deps\rjson.dll: src\lib.rs
+
+C:\Users\ashu1\Coding Projects\PortMortem cJSON\cJSONtoRust\rJSON\target\debug\deps\librjson.rlib: src\lib.rs
+
+src\lib.rs:
---
+++ rJSON/target/debug/rjson.d
@@ -0,0 +1,1 @@
+C:\Users\ashu1\Coding\ Projects\PortMortem\ cJSON\cJSONtoRust\rJSON\target\debug\rjson.dll: C:\Users\ashu1\Coding\ Projects\PortMortem\ cJSON\cJSONtoRust\rJSON\src\lib.rs
